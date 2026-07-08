637ed6760de8421d4cfa3397a6e7f1a7b174002c
AliND <alinematdoost82@gmail.com>
2026-07-08 21:34:49 +0330

    new features added to parser

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import re
 from collections import Counter
 import sys
+import math
 
-def parser(path):
+def parser(path, threshold):
     pattern = r'^(\S+) .*? \[(.*?)\] "(\S+) (\S+) \S+" (\d+) (\d+|-)'
 
     corruptedLines = 0
@@ -11,6 +12,13 @@ def parser(path):
     ipSet = set()
     endpointCounter = Counter()
     error = 0
+    hourlyCounter = Counter()
+
+    # this is for tracking 401 errors
+    maliciousIp = dict()
+
+    # this is for tracking 5xx errors
+    internalError = dict()
 
     with open(path, 'r') as file:
         for line in file:
@@ -28,12 +36,24 @@ def parser(path):
                 endpoint = match.group(4)
                 statusCode = match.group(5)
                 sizeInBytes = match.group(6)
+                hour = time.split(':')[1]
 
                 ipSet.add(ip)
                 endpointCounter[endpoint] += 1
+                hourlyCounter[hour] += 1
 
                 if (statusCode.startswith('4') or statusCode.startswith('5')):
                     error += 1
+                    if statusCode == '401' and endpoint == '/login':
+                        if ip in maliciousIp:
+                            maliciousIp[ip] += 1
+                        else:
+                            maliciousIp[ip] = 1
+                    elif statusCode.startswith('5'):
+                        if hour in internalError:
+                            internalError[hour] += 1
+                        else:
+                            internalError[hour] = 1
 
             except ValueError:
                 corruptedLines += 1
@@ -49,10 +69,28 @@ def parser(path):
         print(f"Error rate: {error / correctLines * 100:.2f}%")
     else:
         print("Error rate: 0.00%")
+    print(hourlyCounter)
+
+    for hour, count in hourlyCounter.items():
+        print(f"Hour: {hour} " + f"  |  Requests count: {count}  |  ")
+        print("-------------")
+
+    for ip, count in maliciousIp.items():
+        if count > threshold:
+            print(f"Ip: {ip} had {count} accesses to login with 401 status code")
+
+    for hour, count in internalError.items():
+        if count > 500:
+            print(f"hour {hour} has had {count} number of 5xx errors")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Enter the path to file")
     else:
         path = sys.argv[1]
-        parser(path)
+
+        threshold = 50
+        if len(sys.argv) > 2:
+            threshold = int(sys.argv[2])
+
+        parser(path, threshold)
